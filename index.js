b66ed0c7b4e1335ce70a7be8acb0a33fd8daa0f3
Ryan <griffinryan77@gmail.com>
2023-12-21 20:34:33 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement("H1");
+newHeader.id = 'victory';
+newHeader.textContent = 'Ryan G is the champion';
+document.body.append(newHeader);
